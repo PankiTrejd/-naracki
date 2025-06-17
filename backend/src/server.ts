8bcebeae4ae19ecd51ab6291cf4094c161b5acab
Kris <cncdecorates@gmail.com>
2025-06-17 15:04:32 +0200

    fix: Explicitly configure CORS for Vercel frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,9 @@ const app = express();
 const port = process.env.PORT || 3001; // Backend will run on port 3001
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://naracki.vercel.app',
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
